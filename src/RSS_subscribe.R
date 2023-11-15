faad79d779450ac928a441968fbd5ec5068457dc
wangtien <r05621114@g.ntu.edu.tw>
2023-11-15 23:10:28 +0100

    split the feeding list out

--- src/RSS_subscribe.R
+++ src/RSS_subscribe.R
@@ -1,59 +1,6 @@
 rm(list=ls())
 pacman::p_load(dplyr,purrr,foreach)
-myfeeds <- data.frame(
-  feed_title =c(
-    "Field Crops Research",                
-    "Journal of Experimental Botany"      ,
-    "European Journal of Agronomy"        ,
-    "Frontiers in Plant Science"          ,
-    "Theoretical and Applied Genetics"    ,
-    "Trends in Plant Science"             ,
-    "Crop Science"                        ,
-    "Euphytica"                           ,
-    "Plant and Soil"                    ,  
-    "Agricultural and Forest Meteorology" ,
-    "New Phytologist"                     ,
-    "Plant, Cell & Environment"         ,
-    "Agricultural Systems"                ,
-    "Agronomy for Sustainable Development",
-    "Nature","Nature food","Nature plant",    "Science",
-    "scientific report",
-    "Trends in Ecology & Evolution",
-    "PNAS bio","PNAS agri","PNAS eco","PNAS env","PNAS evo","PNAS gene",
-    "Annual plant commuciation",
-    "Annual Review of Plant Biology"
-  ),
-  feed_url =c("https://rss.sciencedirect.com/publication/science/03784290",
-              "https://academic.oup.com/rss/site_5304/3170.xml",
-              "https://rss.sciencedirect.com/publication/science/11610301",
-              "https://www.frontiersin.org/journals/plant-science/rss",
-              "https://link.springer.com/search.rss?facet-content-type=Article&facet-journal-id=122&channel-name=Theoretical%20and%20Applied%20Genetics",
-              "https://rss.sciencedirect.com/publication/science/13601385",
-              "https://acsess.onlinelibrary.wiley.com/action/showFeed?jc=14350653&type=etoc&feed=rss",
-              "https://link.springer.com/search.rss?facet-content-type=Article&facet-journal-id=10681&channel-name=Euphytica",
-              "https://link.springer.com/search.rss?facet-journal-id=11104&package=openaccessarticles&search-within=Journal&query=",
-              "https://rss.sciencedirect.com/publication/science/01681923",
-              "https://onlinelibrary.wiley.com/feed/14698137/most-recent",
-              "https://onlinelibrary.wiley.com/feed/13653040/most-recent",
-              "https://rss.sciencedirect.com/publication/science/0308521X",
-              "https://link.springer.com/search.rss?facet-journal-id=13593&package=openaccessarticles&search-within=Journal&query=",
-              "https://www.nature.com/nature.rss",
-              "https://www.nature.com/natfood.rss",
-              "https://www.nature.com/nplants.rss",
-              "https://www.science.org/action/showFeed?type=etoc&feed=rss&jc=science",
-              "https://www.nature.com/srep.rss",
-              "http://www.cell.com/trends/ecology-evolution/current.rss",
-              "https://www.pnas.org/action/showFeed?type=searchTopic&taxonomyCode=topic&tagCode=bio-sci",
-              "https://www.pnas.org/action/showFeed?type=searchTopic&taxonomyCode=topic&tagCode=ag-sci",
-              "https://www.pnas.org/action/showFeed?type=searchTopic&taxonomyCode=topic&tagCode=eco",
-              "https://www.pnas.org/action/showFeed?type=searchTopic&taxonomyCode=topic&tagCodeOr=env-sci-bio&tagCodeOr=env-sci-soc&tagCodeOr=env-sci-phys",
-              "https://www.pnas.org/action/showFeed?type=searchTopic&taxonomyCode=topic&tagCode=evolution",
-              "https://www.pnas.org/action/showFeed?type=searchTopic&taxonomyCode=topic&tagCode=genetics",
-              "https://www.annualreviews.org/action/showFeed?ui=45mu4&mi=3fndc3&ai=s0&jc=ecolsys&type=etoc&feed=atom%20",
-              "https://www.annualreviews.org/r/arplant_rss"
-  )
-) %>% 
-  arrange(feed_title)
+source("src/feed_list.R")
 
 wrangle_feed <- function(the_feed_url, the_feed_dataframe = myfeeds) {
   my_feed_data<- tryCatch({
