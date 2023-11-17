77d67c09220d22493d8f9e310126ea982a7c7fb8
wangtien <r05621114@g.ntu.edu.tw>
2023-11-17 22:04:39 +0100

    rerun schedule task

--- src/schedule_task.R
+++ src/schedule_task.R
@@ -2,15 +2,6 @@ myscript <- paste0(getwd(),"/src/RSS_subscribe.R")
 
 taskscheduleR::taskscheduler_create(taskname = "getRSS", 
                      rscript = myscript, 
-                     schedule = "DAILY", starttime = "12:00",
-                     startdate = format(Sys.Date()+1, "%m/%d/%Y"))
+                     schedule = "DAILY", starttime = "12:04",
+                     startdate = format(Sys.Date(), "%m/%d/%Y"))
 taskscheduleR::taskscheduler_delete(taskname = "getRSS")
-
-taskscheduleR::taskscheduler_delete(taskname = "getRSSt")
-
-taskscheduleR::taskscheduler_create(taskname = "getRSSt", 
-                                    rscript = myscript, 
-                                    schedule = "DAILY", 
-                                    starttime = "12:30",
-                                    startdate = format(Sys.Date(), "%m/%d/%Y")
-                                    )
