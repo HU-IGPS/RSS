ccb4f55f971dd9f09acdae3ef3a300d31af800f6
Tien-Cheng <39258210+Illustratien@users.noreply.github.com>
2025-02-14 10:08:53 +0100

    change to upon calling

--- src/schedule_task.R
+++ src/schedule_task.R
@@ -1,7 +1,9 @@
-myscript <- paste0(getwd(),"/src/RSS_subscribe.R")
+myscript <- paste0(getwd(),"/RSS_subscribe.R")
 
 taskscheduleR::taskscheduler_create(taskname = "getRSS", 
                      rscript = myscript, 
-                     schedule = "DAILY", starttime = "12:04",
+                     schedule = "ONCE", 
+                     starttime = format(Sys.time() + 62, "%H:%M"),
                      startdate = format(Sys.Date(), "%m/%d/%Y"))
+
 taskscheduleR::taskscheduler_delete(taskname = "getRSS")
